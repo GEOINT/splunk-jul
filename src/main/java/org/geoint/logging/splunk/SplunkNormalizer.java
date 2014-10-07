61a96b33955fd0efbdaf6aa8af869f01be875058
steve_siebert <steve_siebert@GORD14NWKIGD097>
2014-10-07 12:36:58 -0400

    added field normalizers

---
+++ src/main/java/org/geoint/logging/splunk/SplunkNormalizer.java
@@ -0,0 +1,16 @@
+package org.geoint.logging.splunk;
+
+/**
+ * Normalizes a splunk event field name or value.
+ */
+//@FunctionalInterface
+public interface SplunkNormalizer {
+
+    /**
+     * Normalizes (escape, replace, normalize) a field String.
+     *
+     * @param raw
+     * @return
+     */
+    String normalize(String raw);
+}
--- src/main/java/org/geoint/logging/splunk/StandardSplunkFormatter.java
+++ src/main/java/org/geoint/logging/splunk/StandardSplunkFormatter.java
@@ -1,7 +1,9 @@
 package org.geoint.logging.splunk;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Map.Entry;
 import java.util.TimeZone;
 import java.util.logging.Formatter;
@@ -38,9 +40,10 @@ public class StandardSplunkFormatter extends Formatter {
     private static final char JSON_CLOSE_OBJ = '}';
     private static final char JSON_KV_SEPARATOR = ':';
     private static final char JSON_FIELD_SEPARATOR = ',';
+    private static final char JSON_QUOTE = '\'';
     private static final char CLASS_METHOD_SEPARATOR = '#';
     private static final char KV_SEPARATOR = '=';
-    private static final String FIELD_SEPARATOR = " ";
+    private static final String FIELD_SEPARATOR = ", ";
     private static final char QUOTE = '"';
     private static final String DATE_FORMAT = "yyyy-MM-dd hh:mm:ss.SSS Z";
     private static final ThreadLocal<SimpleDateFormat> dateFormat
@@ -52,36 +55,48 @@ public class StandardSplunkFormatter extends Formatter {
                     return format;
                 }
             };
+    private final List<SplunkNormalizer> normalizers = new ArrayList<>();
+
+    public StandardSplunkFormatter() {
+        //add default normalizers
+        //TODO replace these with lambdas
+
+        //replaces double quotes with single quotes
+        normalizers.add(new SplunkNormalizer() {
+
+            @Override
+            public String normalize(String raw) {
+                return raw.replace("\"", "'");
+            }
+        });
+    }
+
+    /**
+     * Add a normalizer to the chain.
+     *
+     * @param normalizer
+     * @return
+     */
+    public StandardSplunkFormatter normalizer(SplunkNormalizer normalizer) {
+        normalizers.add(normalizer);
+        return this;
+    }
 
     @Override
     public String format(LogRecord lr) {
         StringBuilder sb = new StringBuilder();
-        sb.append(dateFormat.get().format(new Date(lr.getMillis())))
-                .append(FIELD_SEPARATOR)
-                //log level
-                .append(KEY_LEVEL).append(KV_SEPARATOR)
-                .append(QUOTE).append(lr.getLevel().getName()).append(QUOTE)
-                .append(FIELD_SEPARATOR)
-                //logger name
-                .append(KEY_LOGGER).append(KV_SEPARATOR)
-                .append(QUOTE).append(lr.getLoggerName()).append(QUOTE)
-                .append(FIELD_SEPARATOR)
-                //message
-                .append(KEY_MSG).append(KV_SEPARATOR)
-                .append(QUOTE).append(formatMessage(lr)).append(QUOTE)
-                .append(FIELD_SEPARATOR)
-                //log source class+method
-                .append(KEY_SOURCE).append(KV_SEPARATOR)
-                .append(QUOTE).append(lr.getSourceClassName())
-                .append(CLASS_METHOD_SEPARATOR).append(lr.getSourceMethodName())
-                .append(QUOTE);
+        sb.append(dateFormat.get().format(new Date(lr.getMillis())));
+
+        appendKV(sb, KEY_LEVEL, lr.getLevel().getName());//log level
+        appendKV(sb, KEY_LOGGER, lr.getLoggerName());  //logger name
+        appendKV(sb, KEY_MSG, formatMessage(lr)); //message
+        appendKV(sb, KEY_SOURCE, lr.getSourceClassName()
+                + CLASS_METHOD_SEPARATOR
+                + lr.getSourceMethodName()); //log source class+method
 
         if (lr instanceof SplunkLogRecord) {
             for (Entry<String, String> field : ((SplunkLogRecord) lr).getFields()) {
-                sb.append(FIELD_SEPARATOR)
-                        .append(field.getKey())
-                        .append(KV_SEPARATOR)
-                        .append(field.getValue());
+                appendKV(sb, normalize(field.getKey()), field.getValue());
             }
         }
 
@@ -97,42 +112,54 @@ public class StandardSplunkFormatter extends Formatter {
         return sb.toString();
     }
 
+    private void appendKV(StringBuilder sb, String key, String value) {
+        sb.append(FIELD_SEPARATOR)
+                .append(key) //normally don't need to normalize keys -- do this manually as needed
+                .append(KV_SEPARATOR)
+                .append(QUOTE)
+                .append(normalize(value))
+                .append(QUOTE);
+    }
+
+    /**
+     * "Escapes" splunk field IAW their best practices.
+     *
+     * Escaping, for splunk, is actually substitutions.
+     *
+     * @param value
+     */
+    private String normalize(String value) {
+        //TODO 1.8 use streams
+        for (SplunkNormalizer n : normalizers) {
+            value = n.normalize(value);
+        }
+        return value;
+    }
+
     private void formatException(Throwable ex, StringBuilder sb) {
         //exception class type is sometimes all we got
-        sb.append(FIELD_SEPARATOR)
-                .append(KEY_EXCEPTION_CLASS).append(KV_SEPARATOR)
-                .append(QUOTE).append(ex.getClass().getName()).append(QUOTE);
+        appendKV(sb, KEY_EXCEPTION_CLASS, ex.getClass().getName());
 
         if (ex.getMessage() != null && !ex.getMessage().isEmpty()) {
-            sb.append(FIELD_SEPARATOR)
-                    .append(KEY_EXCEPTION_MSG).append(KV_SEPARATOR)
-                    .append(QUOTE).append(ex.getMessage()).append(QUOTE);
+            appendKV(sb, KEY_EXCEPTION_MSG, ex.getMessage());
         }
+
         StackTraceElement[] stack = ex.getStackTrace();
         if (stack != null && stack.length > 0) {
-            sb.append(FIELD_SEPARATOR).append(KEY_STACK).append(KV_SEPARATOR)
+            sb.append(FIELD_SEPARATOR)
+                    .append(KEY_STACK)
+                    .append(KV_SEPARATOR)
+                    .append(QUOTE)
                     .append(JSON_OPEN_ARRAY);
             for (int i = 0; i < stack.length; i++) {
                 StackTraceElement ste = stack[i];
 
                 //store stack trace data as JSON object
-                sb.append(JSON_OPEN_OBJ)
-                        //class name
-                        .append(QUOTE).append(KEY_STACK_CLASS).append(QUOTE)
-                        .append(JSON_KV_SEPARATOR)
-                        .append(QUOTE).append(ste.getClassName()).append(QUOTE)
-                        .append(JSON_FIELD_SEPARATOR)
-                        //method name
-                        .append(QUOTE).append(KEY_STACK_METHOD).append(QUOTE)
-                        .append(JSON_KV_SEPARATOR)
-                        .append(QUOTE).append(ste.getMethodName()).append(QUOTE)
-                        .append(JSON_FIELD_SEPARATOR)
-                        //line number
-                        .append(QUOTE).append(KEY_STACK_LINE).append(QUOTE)
-                        .append(JSON_KV_SEPARATOR)
-                        .append(QUOTE).append(ste.getLineNumber()).append(QUOTE)
-                        //close object
-                        .append(JSON_CLOSE_OBJ);
+                sb.append(JSON_OPEN_OBJ);
+                appendJsonKv(sb, KEY_STACK_CLASS, ste.getClassName()); //class name
+                appendJsonKv(sb, KEY_STACK_METHOD, ste.getMethodName()); //method name
+                appendJsonKv(sb, KEY_STACK_LINE, String.valueOf(ste.getLineNumber())); //line number
+                sb.append(JSON_CLOSE_OBJ);//close object
 
                 if (i + 1 != stack.length) {
                     //more stack
@@ -140,7 +167,18 @@ public class StandardSplunkFormatter extends Formatter {
                 }
             }
             sb.append(JSON_CLOSE_ARRAY);
+            sb.append(QUOTE);
         }
+    }
+
+    private void appendJsonKv(StringBuilder sb, String name, String value) {
+        sb.append(JSON_QUOTE)
+                .append(name)
+                .append(JSON_QUOTE)
+                .append(JSON_KV_SEPARATOR)
+                .append(JSON_QUOTE)
+                .append(value) //TODO escape for json + splunk
+                .append(JSON_QUOTE);
     }
 
 }
